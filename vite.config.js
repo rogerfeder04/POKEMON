f463d16ce88daa0f406ae23ad2cbe161f7a7ed65
rogerfeder04 <rogerfeder0437@gmail.com>
2024-06-08 20:44:08 -0500

    actualizar

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base: '/POKEMON/', //aca se pone el nombre del repositorio en github
+  //aca se pone el nombre del repositorio en github
   plugins: [
     vue({
     template: { transformAssetUrls },
@@ -15,8 +15,9 @@ export default defineConfig({
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: 'src/quasar-variables.sass'
-    })
+    }),
    
 
-  ]
+  ],
+  base: '/POKEMON/'
 })
